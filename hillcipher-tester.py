e9b014d68f44142f9b63b609b0c70db732fcf131
Faizar Ali <alifaizar@gmail.com>
2022-04-13 19:57:40 -0400

    Modified bash script

--- hillcipher-tester.py
+++ hillcipher-tester.py
@@ -6,28 +6,28 @@ def compile_c(c_file):
     cmd = 'gcc ' + c_file
     process = subprocess.Popen(shlex.split(cmd))
     process.wait()
-    return "./a.out"
+    return './a.out'
 
 def compile_cpp(cpp_file):
     cmd = 'g++ ' + cpp_file
     process = subprocess.Popen(shlex.split(cmd))
     process.wait()
-    return "./a.out"
+    return './a.out'
 
 def compile_java(java_file):
     cmd = 'javac ' + java_file
     process = subprocess.Popen(shlex.split(cmd))
     process.wait()
-    return "java hillcipher"
+    return 'java hillcipher'
 
 def compile_go(go_file):
     cmd = 'go build ' + go_file
     process = subprocess.Popen(shlex.split(cmd))
     process.wait()
-    return "./hillcipher"
+    return './hillcipher'
 
 def compile_py(py_file):
-    return "python3 hillcipher.py"
+    return 'python3 hillcipher.py'
 
 if len(sys.argv) != 2:
     print('Usage: python3 hillcipher-tester.py [filename]')
@@ -35,31 +35,31 @@ if len(sys.argv) != 2:
 
 arg = sys.argv[1]
 
-if arg == "hillcipher.c":
+if arg == 'hillcipher.c':
     EXE = compile_c(arg)
-elif arg == "hillcipher.cpp":
+elif arg == 'hillcipher.cpp':
     EXE = compile_cpp(arg)
-elif arg == "hillcipher.java":
+elif arg == 'hillcipher.java':
     EXE = compile_java(arg)
-elif arg == "hillcipher.go":
+elif arg == 'hillcipher.go':
     EXE = compile_go(arg)
-elif arg == "hillcipher.py":
+elif arg == 'hillcipher.py':
     EXE = compile_py(arg)
 else:
-    print("Invalid source file name")
+    print('Invalid source file name')
     sys.exit(1)
 
 test_case_num = 7
 
 # Accounts for seven test cases
 for i in range(1, test_case_num + 1):
-    print(f"Case #{i}")
+    print(f'Case #{i}')
     input = EXE + f' k{i}.txt p{i}.txt'
     command = shlex.split(input)
-    with open(f'myOutput{i}.txt', "w") as outfile:
+    with open(f'myOutput{i}.txt', 'w') as outfile:
         p = subprocess.Popen(command, stdout=outfile)
         p.wait()
     input = f'diff myOutput{i}.txt sample_output/k{i}p{i}.txt'
     p = subprocess.Popen(shlex.split(input))
     p.wait()
-    print(f"Case #{i} - complete")
+    print(f'Case #{i} - complete')
--- hillcipher-tester.sh
+++ hillcipher-tester.sh
@@ -28,37 +28,12 @@ hillcipher.py)
 	exit 1
 esac
 
-echo "Case #1"
-eval $EXE k1.txt p1.txt > myOutput1.txt
-diff myOutput1.txt sample_output/k1p1.txt
-echo "Case #1 - complete"
+test_case_num=7
 
-echo "Case #2"
-eval $EXE k2.txt p2.txt > myOutput2.txt
-diff myOutput2.txt sample_output/k2p2.txt
-echo "Case #2 - complete"
-
-echo "Case #3"
-eval $EXE k3.txt p3.txt > myOutput3.txt
-diff myOutput3.txt sample_output/k3p3.txt
-echo "Case #3 - complete"
-
-echo "Case #4"
-eval $EXE k4.txt p4.txt > myOutput4.txt
-diff myOutput4.txt sample_output/k4p4.txt
-echo "Case #4 - complete"
-
-echo "Case #5"
-eval $EXE k5.txt p5.txt > myOutput5.txt
-diff myOutput5.txt sample_output/k5p5.txt
-echo "Case #5 - complete"
-
-echo "Case #6"
-eval $EXE k6.txt p6.txt > myOutput6.txt
-diff myOutput6.txt sample_output/k6p6.txt
-echo "Case #6 - complete"
-
-echo "Case #7"
-eval $EXE k7.txt p7.txt > myOutput7.txt
-diff myOutput7.txt sample_output/k7p7.txt
-echo "Case #7 - complete"
+for ((i = 1; i <= test_case_num; i++))
+do
+	echo "Case #$i"
+    eval $EXE k$i.txt p$i.txt > myOutput$i.txt
+    diff myOutput$i.txt sample_output/k${i}p${i}.txt
+    echo "Case #$i - complete"
+done
